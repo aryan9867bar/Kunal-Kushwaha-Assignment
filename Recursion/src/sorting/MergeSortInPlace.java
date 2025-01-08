040b8a673063a9be328fb9596aafabb1fff6f95f
Aryan Baranwal <aryan123web@gmail.com>
2025-01-08 09:14:36 +0545

    merge sort and quick sort

---
+++ Recursion/src/sorting/MergeSortInPlace.java
@@ -0,0 +1,57 @@
+package sorting;
+
+import java.util.*;
+
+public class MergeSortInPlace {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSortInPlace(int[] arr, int start, int end) {
+        if (end - start == 1) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        mergeSortInPlace(arr, start, mid);
+        mergeSortInPlace(arr, mid, end);
+
+        mergeInPlace(arr, start, mid, end);
+    }
+
+    static void mergeInPlace(int[] arr, int start, int mid, int end) {
+        int[] mix = new int[end - start];
+        int i = start;
+        int j = mid;
+        int k = 0;
+
+        while (i < mid && j < end) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < mid) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j < end) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (int l = 0; l < mix.length; l++) {
+            arr[start + l] = mix[l];
+        }
+    }
+}
---
+++ Recursion/src/sorting/QuickSort.java
@@ -0,0 +1,40 @@
+package sorting;
+
+import java.util.*;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] nums, int low, int hi) {
+        if (low >= hi) {
+            return;
+        }
+        int start = low;
+        int end = hi;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+
+        while (start <= end) {
+            // also a reason why if its already sorted it will not swap
+            while (nums[start] < pivot) {
+                start++;
+            }
+            while (nums[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        // now my pivot is at correct index, please sort two halves now
+        sort (nums, low, end);
+        sort (nums, start, hi);
+    }
+}
